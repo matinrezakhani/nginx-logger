f55392014e6af1c20e9903f60f54ceb0af25d24d
Matin Rezakhani <matin.rezakhani@gmail.com>
2021-06-14 09:21:29 +0430

    add frontend logger function

---
+++ logger_front/logger.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+/**
+ *
+ * @param {('info'|'warn'|'error')} type
+ * @param {string} location
+ * @param {string} message
+ */
+export async function logger(type = "info", location, message) {
+  if (type != "info" && "warn" && "error"){
+      console.error('type of log is incorrect')
+  };
+  let loggerAddress = "/logger";
+  if (process.env.NODE_ENV === "development") {
+    loggerAddress = "http://127.0.0.1:8080/logger";
+  }
+  try {
+    let log = {
+      data: message,
+      type: type,
+      location: location,
+      userAgent: navigator.userAgent,
+    };
+
+    log = btoa(JSON.stringify(log));
+    await axios.post(loggerAddress , {log : log})
+  } catch (error) {
+      console.error('logger-' , error )
+  }
+}
