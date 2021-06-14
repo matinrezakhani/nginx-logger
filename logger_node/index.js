db9f6c5fc999bcfbd0e00bca2c670b7e682dc7ed
Matin Rezakhani <matin.rezakhani@gmail.com>
2021-06-14 08:37:34 +0430

    change logger package

--- logger_node/index.js
+++ logger_node/index.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const logger = require("node-color-log");
+const { createLogger, format, transports } = require("winston");
+const { combine, splat, timestamp, printf } = format;
 
 const port = 5000;
 const app = express();
+
+const myFormat = printf(({ level, message, timestamp, ...metadata }) => {
+  let msg = `${timestamp} [${level}] ${message} `;
+  return msg;
+});
+
+const logger = createLogger({
+  format: combine(format.colorize(), splat(), timestamp(), myFormat),
+  transports: [new transports.Console({ level: "info" })],
+});
 
 app.use(cors());
 app.use(bodyParser.json()), app.use(bodyParser.urlencoded({ extended: false }));
@@ -14,13 +25,16 @@ app.post("/", (req, res) => {
     let buff = new Buffer.from(log, "base64");
     log = buff.toString("ascii");
     log = JSON.parse(log);
-    let logMessage ='FROM: '+req.headers["user-agent"] + " : " + log.location +" => " + log.data
+    let logMessage =
+      "FROM: " +
+      req.headers["user-agent"] +
+      " : " +
+      log.location +
+      " => " +
+      log.data;
     switch (log.type) {
       case "error":
         logger.error(logMessage);
-        break;
-      case "debug":
-        logger.debug(logMessage);
         break;
       case "warn":
         logger.warn(logMessage);
@@ -30,12 +44,12 @@ app.post("/", (req, res) => {
         break;
     }
   } catch (error) {
-    logger.error(error);
+    logger.error('Logger: ',error);
   }
 
   res.status(200).json("success");
 });
 
 app.listen(port, () => {
-  logger.info(`logger listening at */logger`);
+  logger.info("logger listening at */logger");
 });
