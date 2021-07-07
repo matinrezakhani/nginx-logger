87c7114e859ac2c5c3a5920532805522b682d775
Matin Rezakhani <matin.rezakhani@gmail.com>
2021-07-07 22:51:03 +0430

    update logger api url

--- logger_front/logger.js
+++ logger_front/logger.js
@@ -10,9 +10,9 @@ export async function logger(type = "info", location, message) {
   if (type != "info" && "warn" && "error"){
       console.error('type of log is incorrect')
   };
-  let loggerAddress = `${window.location.origin}/logger`
+  let loggerAddress = `${window.location.origin}/api/logger`
   if (process.env.NODE_ENV === "development") {
-    loggerAddress = "http://127.0.0.1:8080/logger";
+    loggerAddress = "http://127.0.0.1:8080/api/logger";
   }
   try {
     let log = {
